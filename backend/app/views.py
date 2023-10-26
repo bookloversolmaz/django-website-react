5648c4aec171b00b870e8daa5e8a2b6d17930d62
bookloverbabe <solmaz@hotmail.co.uk>
2023-10-26 15:18:57 +0100

    Fixed views.py so that user can post info to database

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
+# from django.shortcuts import render
+# from rest_framework.views import APIView
+# from .models import *
+# from .serializer import *
+# from rest_framework.response import Response
+
+# class ReactView(APIView):
+#     def get(self, request):
+#         output = [{"item": output.item,
+#                    "description": output.description}
+#                 for output in React.objects.all()]
+#         return Response(output)
+    
+#     def post(self, request):
+#         serializer = ReactSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data, status=200)
+#         else:
+#             return Response.status_code(500)
+
 from .models import *
 from .serializer import *
-from rest_framework.response import Response
+from django.shortcuts import render
+from rest_framework import generics
 
-class ReactView(APIView):
-    def get(self, request):
-        output = [{"item": output.item,
-                   "description": output.description}
-                for output in React.objects.all()]
-        return Response(output)
-    
-    def post(self, request):
-        serializer = ReactSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data)
+class ReactView(generics.ListCreateAPIView):
+    queryset = React.objects.all()
+    serializer_class = ReactSerializer
