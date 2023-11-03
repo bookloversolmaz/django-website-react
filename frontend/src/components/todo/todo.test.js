ca5d700a18ebfde691184bbf2ae4b3f50bf6f51b
bookloverbabe <solmaz@hotmail.co.uk>
2023-11-03 12:20:24 +0000

    Passes second test

--- frontend/src/components/todo/todo.test.js
+++ frontend/src/components/todo/todo.test.js
@@ -1,26 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import axios from 'axios';
-import ToDo from './todo.js';
+import { ToDo, componentDidMount } from './todo.js';
 
 // Create jest.mock function to automatically mock the axios module
 jest.mock('axios');
 
+// Creates mocks to check that it can fetch data
 test('it should fetch to do list data', () => {
   const ToDo = [{item: 'Clean', description: 'Do laundry'}];
   const response = {data: ToDo};
   axios.get.mockImplementation(() => Promise.resolve(response))
 });
 
-// Testing for async errors using Promise.catch.
-// it('tests error with promises', () => {
-//   expect.assertions(2);
-//   return componentDidMount().catch(e =>
-//     expect(e).toEqual({
-//       error: 'Error fetching data:',
-//     }),
-//   );
-// });
-
+// Checks that it correctly throws an error if it fails to retrieve data
 test('the fetch fails with an error', async () => {
-  await expect(ToDo.componentDidMount()).rejects.toMatch('Error fetching data:');
+  await expect(Promise.reject(new Error('Error fetching data:'))).rejects.toThrow('Error fetching data:');
 });
