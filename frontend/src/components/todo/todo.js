fcc1177637d14f926062846edeeb8620a8c34e57
bookloverbabe <solmaz@hotmail.co.uk>
2023-11-14 16:41:26 +0000

    Added usestate

--- frontend/src/components/todo/todo.js
+++ frontend/src/components/todo/todo.js
@@ -1,12 +1,40 @@
 import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
 
-class ToDo extends React.Component {
- // Create state that remembers details i.e. the database info as an array
-  state = {details: [], }
+// class ToDo extends React.Component {
+//  // Create state that remembers details i.e. the database info as an array
+//   state = {details: [], }
 
-// React will the below when the component is added (mounted) to the screen, calls data from backend using axios API.
-  componentDidMount() {
+// // React will the below when the component is added (mounted) to the screen, calls data from backend using axios API.
+//   componentDidMount() {
+
+
+
+
+  // Respond to the event. Create an event handler (functions that are triggered by user interactions)
+  // To add an event handler, you will first define a function and then pass it as a prop to the appropriate JSX tag. One event is a 
+  // form and another a button to submit the entries
+  // Trigger a render (delivering the diner's order to the kitchen)
+const todoForm = ({ queryset }) => {
+  // UseState hook updates the component with new data between renders
+  const [item, setItem] = useState("");
+  const [description, setDescription] = useState("");
+  // TODO: create another component allows user to edit to do list in frontend e.g, read, add, delete, update.
+
+  // Rendering the component (preparing the order in the kitchen)
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  // Committing to the DOM (placing the order on the table)
+  // TODO: ensure that changes made by the user updates the database in the backend
+  // TODO: Fix error, whereby each time the backend is reloaded, each entry is re-rendered in the frontend
+
+  // Renders the below using the information gathered from the backend
+
+
+
+
+
     let data;
     // The axios.get function returns a Promise
     axios.get('http://localhost:8000')
@@ -22,36 +50,31 @@ class ToDo extends React.Component {
       console.error('Error fetching data:', error);
     });
   }
-  // TODO: create another component allows user to edit to do list in frontend e.g, read, add, delete, update.
-  // Respond to the event. Create an event handler (functions that are triggered by user interactions)
-  // To add an event handler, you will first define a function and then pass it as a prop to the appropriate JSX tag. One event is a 
-  // form and another a button to submit the entries
 
-  // Trigger a render (delivering the diner's order to the kitchen)
+  return (
+    <>
+    <header>My website</header>
+      <form action={handleSubmit}>
+      <input name="query" />
+      <button type="submit">Search</button>
+    </form>
+    </>
+  )
 
-  // Rendering the component (preparing the order in the kitchen)
-
-  // Committing to the DOM (placing the order on the table)
-  // TODO: ensure that changes made by the user updates the database in the backend
-  // TODO: Fix error, whereby each time the backend is reloaded, each entry is re-rendered in the frontend
-
-  // Renders the below using the information gathered from the backend
-  render() {
-    return (
-      <div>
-        <header>My website</header>
-        <hr></hr>
-        {this.state.details.map((queryset, id) => (
-          <div data-testid='todo-1' key={id}> {" "}
-          <div>
-            <h2>{queryset.item}</h2>
-            <h3>{queryset.description}</h3>
-          </div>
-          </div>
-        ))}
-      </div>
-    )
-  }
+    // return (
+    //   <div>
+    //     <header>My website</header>
+    //     <hr></hr>
+    //     {this.state.details.map((queryset, id) => (
+    //       <div data-testid='todo-1' key={id}> {" "}
+    //       <div>
+    //         <h2>{queryset.item}</h2>
+    //         <h3>{queryset.description}</h3>
+    //       </div>
+    //       </div>
+    //     ))}
+    //   </div>
+    // )
 }
 
-export default ToDo;
+export default todoForm;
