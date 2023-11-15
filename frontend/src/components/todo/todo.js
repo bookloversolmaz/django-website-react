b4020631ee6db654698df194c4b5b135240bf573
bookloverbabe <solmaz@hotmail.co.uk>
2023-11-15 19:01:28 +0000

    Finished for today

--- frontend/src/components/todo/todo.js
+++ frontend/src/components/todo/todo.js
@@ -10,18 +10,14 @@ import { useState } from 'react';
 //   componentDidMount() {
 
 
-
-
   // Respond to the event. Create an event handler (functions that are triggered by user interactions)
   // To add an event handler, you will first define a function and then pass it as a prop to the appropriate JSX tag. One event is a 
   // form and another a button to submit the entries
   // Trigger a render (delivering the diner's order to the kitchen)
-const TodoForm = ({ queryset }) => {
-  // UseState hook updates the component with new data between renders
-  const [item, setItem] = useState("");
-  const [description, setDescription] = useState("");
-  // TODO: create another component allows user to edit to do list in frontend e.g, read, add, delete, update.
 
+  // UseState hook updates the component with new data between renders
+  // TODO: create another component allows user to edit to do list in frontend e.g, read, add, delete, update.
+  // const [item, setItem] = useState("");
   // Rendering the component (preparing the order in the kitchen)
   // Committing to the DOM (placing the order on the table)
   // TODO: ensure that changes made by the user updates the database in the backend
@@ -30,34 +26,27 @@ const TodoForm = ({ queryset }) => {
   // Renders the below using the information gathered from the backend
 
 
-    let data;
-    // The axios.get function returns a Promise
-    axios.get('http://localhost:8000')
-    // When you call .then() on the Promise, you provide a callback function that will be executed when the Promise is resolved (when the data is successfully fetched). 
-    .then( response => {
-      data = response.data;
-      this.setState({
-        details: data
-      });
-    })
-    // .catch() method handles errors if the Promise is rejected (if there's an error during the fetch operation).
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-  }
+  //   let data;
+  //   // The axios.get function returns a Promise
+  //   axios.get('http://localhost:8000')
+  //   // When you call .then() on the Promise, you provide a callback function that will be executed when the Promise is resolved (when the data is successfully fetched). 
+  //   .then( response => {
+  //     data = response.data;
+  //     this.setState({
+  //       details: data ({
+  //         item : item
+  //       })
+  //     });
+  //   })
+  //   // .catch() method handles errors if the Promise is rejected (if there's an error during the fetch operation).
+  //   .catch(error => {
+  //     console.error('Error fetching data:', error);
+  //   });
+  // }
 
-  const handleClick = () => {
-    const id = todoList.length + 1;
-    setTodoList((prev) => [
-      ...prev,
-      {
-        id: id,
-        item: input,
-        complete: false,
-      },
-    ]);
-    setInput("");
-  };
+  // const handleSubmit = (event) => {
+  //   setItem(event.target.value)
+
 
   // return (
   //   <>
@@ -65,12 +54,11 @@ const TodoForm = ({ queryset }) => {
   //     <form action={handleSubmit}>
   //     <input name="queryset.item" />
   //     <button type="submit">Submit</button>
-  //     <input name="queryset.description" />
-  //     <button type="submit">Submit</button>
   //   </form>
   //   </>
-  // )
-
+  // );
+  // }
+  
     // return (
     //   <div>
     //     <header>My website</header>
@@ -87,4 +75,6 @@ const TodoForm = ({ queryset }) => {
     // )
 // }
 
-export default TodoForm;
+
+
+// export default TodoForm;
