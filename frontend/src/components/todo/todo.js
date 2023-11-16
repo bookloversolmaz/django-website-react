db53d03bd1ef5ad1240b10a7c4219997c9adf0c8
bookloverbabe <solmaz@hotmail.co.uk>
2023-11-16 18:25:05 +0000

    Finished for today

--- frontend/src/components/todo/todo.js
+++ frontend/src/components/todo/todo.js
@@ -40,35 +40,56 @@ import { useState } from 'react';
     // )
 // }
 
-
-// TODO: enable user to edit to do list in frontend e.g, read, add, delete, update while also updating the backend
-function CreateToDoList () {
-  // Create state that remembers details i.e. the database info as an array
-  state = {details: [], }
-  // TODO: connect that to the backend and save it to the database
-
-  function ConnectToBackend() {
-    let data;
-    // The axios.get function returns a Promise
-    axios.get('http://localhost:8000')
-    // When you call .then() on the Promise, you provide a callback function that will be executed when the Promise is resolved (when the data is successfully fetched). 
-    .then( response => {
-      data = response.data;
-      this.setState({
-        details: data ({
-          item : item
-        })
-      });
-    })
-    // .catch() method handles errors if the Promise is rejected (if there's an error during the fetch operation).
-    .catch(error => {
-      console.error('Error fetching data:', error);
+class ToDo extends React.Component {
+  // Create initialList that remembers details i.e. the database info as an array
+  initialList =  { details: [], }
+  // TODO: connect to the backend via axios
+  componentDidMount() {
+  let data;
+  // The axios.get function returns a Promise
+  axios.get('http://localhost:8000')
+  // When you call .then() on the Promise, you provide a callback function that will be executed when the Promise is resolved (when the data is successfully fetched). 
+  .then( response => {
+  data = response.data;
+  this.setInitialList({
+    details: data ({
+      item : item
+      })
     });
+  });
+  this.componentDidCatch(error, info)
+  logErrorToMyService(error, info.componentStack);
 
+  
+const [list, setList] = useState(ToDo);
+const [item, setItem] = useState("");
 
-  }
+  return (
+    <>
+      <input
+        value={text}
+        onChange={event => setItem(event.target.value)}
+      />
+  <button onClick={() => {
+        state('');
+        setList([{
+          id: item.length,
+          item: item
+        }, ...item]);
+      }}>Add</button> */
+      <ul>
+        {list.map(data => (
+          <div data-testid='todo-1' key={id}> {" "}
+          <li key={data.id}>
+            {setItem.item}
+          </li>
+          </div>
+        ))}
+      </ul>
+    </>
+  );
+  };
+
 }
-// TODO: create a form where user can enter items
-// TODO: connect that to the backend and save it to the database
-// TODO: render all of the items from the backend
-// TODO: enable user to delete items from the list, which also deletes it from the backend
+
+export default ToDo;
