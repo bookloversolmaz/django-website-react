893b11c178265cec2316d63d79337324ea5270ff
bookloverbabe <solmaz@hotmail.co.uk>
2023-11-17 15:45:28 +0000

    A nearly working frontend

--- frontend/src/components/todo/todo.js
+++ frontend/src/components/todo/todo.js
@@ -1,114 +1,95 @@
 import axios from 'axios';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-// class ToDo extends React.Component {
-//  // Create state that remembers details i.e. the database info as an array
-//   state = {details: [], }
-// // React will the below when the component is added (mounted) to the screen, calls data from backend using axios API.
-//   componentDidMount() {
-  //   let data;
-  //   // The axios.get function returns a Promise
-  //   axios.get('http://localhost:8000')
-  //   // When you call .then() on the Promise, you provide a callback function that will be executed when the Promise is resolved (when the data is successfully fetched). 
-    // .then( response => {
-    //   data = response.data;
-    //   this.setState({
-    //     details: data ({
-    //       item : item
-    //     })
-    //   });
-    // })
-    // // .catch() method handles errors if the Promise is rejected (if there's an error during the fetch operation).
-    // .catch(error => {
-    //   console.error('Error fetching data:', error);
-    // });}
-    // return (
-      // <div>
-      //   <header>My website</header>
-      //   <hr></hr>
-      //   {this.state.details.map((queryset, id) => (
-      //     <div data-testid='todo-1' key={id}> {" "}
-      //     <div>
-      //       <h2>{queryset.item}</h2>
-      //       <h3>{queryset.description}</h3>
-      //     </div>
-      //     </div>
-      //   ))}
-      // </div>
-    // )
-// }
-
-const TodoList = () => {
-  const [inputTask, setInputTask] = useState('');
+// Create a to do list that is connected via the backend database using axios. The list must be editable, whereby the user can read, update and delete items
+const Todo = () => {
+  // Each item in the list
+  const [inputItem, setInputItem] = useState('');
+  // The list itself, initialised to an empty array
   const [list, setList] = useState([]);
+  
+  // Fetch existing data from the backend when the component mounts, before the user updates the list
+  // UseEffect is a React Hook that lets you synchronize a component with an external system.
+  useEffect(() => {
+    fetchData(); 
+  }, []);
 
-  // Connect to the backend, so that the user's actions updates the database containing the information
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    let data;
-    axios.get('http://localhost:8000') 
-    // When you call .then() on the Promise, you provide a callback function that will be executed when the Promise is resolved (when the data is successfully fetched). 
-    .then( response => {
-      data = response.data;
-      this.setState({
-        details: data ({
-          item: inputTask
-        })
-      });
-    })
-
-    // .catch() method handles errors if the Promise is rejected (if there's an error during the fetch operation).
-    .catch(error => {
+  // Reading items. The data for the list is obtained from the database using the axios API
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/app');
+      setList(response.data); // Update the list with data from the backend
+      // A promise that catches an error if the promise is unfulfilled
+    } catch (error) {
       console.error('Error fetching data:', error);
-    });
-  }
-
-  // Add an item to the to do list
-  const handleAddTodo = (handleSubmit) => {
-      const newTask = {
+    }
+  };
+  
+  // Adding items to the list via the database
+  const handleAddTodo = async () => {
+    if (inputItem.trim() !== '') {
+      try {
+        const response = await axios.post('http://localhost:8000/app', {
           id: Math.random(),
-          item: inputTask
-      };
-    //  update the list state by using the spread operator […list] to create a new array with the existing tasks in the list. Append the newTask to the end of the array
-     setList([...list, newTask]);
-    // reset the inputTask state to an empty string, clearing the input field when the user clicks the button.
-      setInputTask('');
+          item: inputItem,
+        });
+        // Update the list state by using the spread operator […list] to create a new array with the existing tasks in the list. 
+        // Append the response.data to the end of the array. Reset the inputItem state to an empty string, clearing the input field when the user clicks the button.
+        setList([...list, response.data]); // Update the list with the new item
+        setInputItem(''); // Reset input field
+      } catch (error) {
+        console.error('Error adding task:', error);
+      }
+    }
   };
-  // Delete item from the list
- const handleDeleteTodo = (handleSubmit, id) => {
-      const newList = list.filter((todo) => todo.id !== id);
-      setList(newList);
+  
+  // Delete items from the list, do the same in the backend
+  const handleDeleteTodo = async (id) => {
+    try {
+      // Make a DELETE request to the backend API with the specific task ID
+      await axios.delete(`http://localhost:8000/app/${id}`);
+      // Delete the item from the list and the backend when the user presses the delete button for that item
+      // Use filter to create a new list that filters out the item with the id that had been deleted
+      const updatedList = list.filter((item) => item.id !== id);
+      setList(updatedList); // Update the list by removing the deleted task
+    } catch (error) {
+      console.error('Error deleting task:', error);
+    }
   };
 
-  // event handler that adds the item to the list when the user licks the button
- const handleInputChange = (event) => {
-      setInputTask(event.target.value);
+  // Event handler which handles all of the changes
+  const handleInputChange = (event) => {
+    setInputItem(event.target.value);
   };
 
- return (
-      <div className="Todo">
-          <h1>My To do list</h1>
+  return (
+    <div className="Todo">
+      <h1>My To-Do List</h1>
 
-          <div className="Top">
-              <input className="input" type="text" value={inputTask}
-                 onChange={handleInputChange} placeholder="Enter a task" />
-
-              <button className="btn" onClick={handleAddTodo}>Submit</button>
-          </div>
-
-         <ul>
-              { list.map((queryset) => (
-                  <li className="task" key={id}>
-                      {queryset.item}
-                      <button onClick={() => handleDeleteTodo(todo.id)}>
-                         Delete
-                     </button>
-                  </li>
-              ))}
-          </ul>
+      <div className="Top">
+        <input
+          className="input"
+          type="text"
+          value={inputItem}
+          onChange={handleInputChange}
+          placeholder="Enter a task"
+        />
+        <button className="btn" onClick={handleAddTodo}>
+          Submit
+        </button>
       </div>
+
+      <ul>
+        {list.map((item) => (
+          <li className="task" key={item.id}>
+            {item.item}
+            <button onClick={() => handleDeleteTodo(item.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
-export default TodoList; 
+export default Todo;
