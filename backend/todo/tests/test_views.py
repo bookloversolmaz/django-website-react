1e609b60884c835fd19f6f7546ce77c65d0fdca9
bookloverbabe <solmaz@hotmail.co.uk>
2023-12-12 19:36:21 +0000

    Passed 4 tests in views in backend

--- backend/todo/tests/test_views.py
+++ backend/todo/tests/test_views.py
@@ -19,21 +19,24 @@ class ReactViewTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
     
-    def test_post(request, pk):
-        # View function after adding an item to the to do list
-        item_instance = get_object_or_404(ItemInstance)
-        # If this is a POST request then process the Form data
-        if request.method == 'POST':
-            # Create a form instance and populate it with data from the request (binding):
-            todo_list = ReactSerializer(request.POST)
-            # Check if the form is valid:
-            if form.is_valid():
-            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-                item_instance = form.cleaned_data['Hoover room']
-                item_instance.save()
+    def test_post(self):
+        # posting data to create a new item using ReactView
+        data = {'item': 'New Test Item'}
+        url = reverse('todo')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Add more assertions based on your application's behavior after a successful POST request
 
-            # redirect to a new URL:
-            return HttpResponseRedirect(reverse('Hoover room'))
+        # item_instance = get_object_or_404(ItemInstance, pk=pk)
+        # # If this is a POST request then process the Form data
+        # if request.method == 'POST':
+        #     # Create a form instance and populate it with data from the request (binding):
+        #     todo_list = ReactSerializer(request.POST)
+        #     # Check if the form is valid:
+        #     if form.is_valid():
+        #     # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+        #         item_instance = form.cleaned_data['Hoover room']
+        #         item_instance.save()
 
-
-
+        #     # redirect to a new URL:
+        #     return HttpResponseRedirect(reverse('Hoover room'))
