19542073c391e12cb64ac5939c1e2bf4061c57c3
bookloverbabe <solmaz@hotmail.co.uk>
2023-12-13 17:40:46 +0000

    Passed all tests for ReactDetail

--- backend/todo/tests/test_views.py
+++ backend/todo/tests/test_views.py
@@ -40,4 +40,40 @@ class ReactViewTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
         # Check if the instance has been deleted from the database
-        # self.assertFalse(React.objects.filter(pk=item_to_delete.pk).exists())
+        self.assertFalse(React.objects.filter(pk=item_to_delete.pk).exists())
+
+
+class ReactDetailTestCase(APITestCase):
+    def setUp(self):
+        # Create test data
+        self.react = React.objects.create(item='Test item')
+
+    def test_get(self):
+        url = reverse('todo')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+    
+    def test_post(self):
+        # posting data to create a new item using ReactView
+        data = {'item': 'New Test Item'}
+        url = reverse('todo')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_delete(self):
+        # # Creates mock objects
+        item_to_delete = React.objects.create(item='Item to Delete')
+
+        # Get the URL for the ReactDetail view for the specific instance
+        url = reverse('todo-delete', kwargs={'pk': item_to_delete.pk})
+
+        # Send a DELETE request to the specified URL
+        response = self.client.delete(url)
+
+        # Check if the response status code is 204 (No Content) upon successful deletion
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # Check if the instance has been deleted from the database
+        self.assertFalse(React.objects.filter(pk=item_to_delete.pk).exists())
