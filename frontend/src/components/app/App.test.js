e6f0a2b9e34a084ed3c7665c0650aba53f928eca
Solmaz Purser <solmaz@hotmail.co.uk>
2024-02-23 17:28:24 +0000

    Passed tests for App.test.js

--- frontend/src/components/app/App.test.js
+++ frontend/src/components/app/App.test.js
@@ -1,8 +1,26 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders home page by default', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
+
+  const homeHeading = screen.getByRole('heading', { name: /Welcome to the Homepage/i });
+  expect(homeHeading).toBeInTheDocument();
+});
+
+test('renders todo page when navigating to /todo', () => {
+  render(
+    <MemoryRouter initialEntries={['/todo']}>
+      <App />
+    </MemoryRouter>
+  );
+
+  const todoHeading = screen.getByRole('heading', { name: /To do list/i });
+  expect(todoHeading).toBeInTheDocument();
+});
