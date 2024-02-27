b286252bd1a80437c1918a5757bbebd021e17686
Solmaz Purser <solmaz@hotmail.co.uk>
2024-02-27 17:34:27 +0000

    Passed test for home.view in backend

--- backend/home/tests.py
+++ backend/home/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+# from django.urls import reverse
+# from django.shortcuts import get_object_or_404
+# from rest_framework.test import APITestCase
+# from rest_framework import status
+# from views import HomeView
 
-# Create your tests here.
+class HomeViewTestCase(TestCase):
+    def test_get(self):
+        response = self.client.get('', follow=True)
+        self.assertEqual(response.status_code, 200)
