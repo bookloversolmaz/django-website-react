27589ad4385500e2f7825811094716f4ed2a8041
Solmaz Purser <solmaz@hotmail.co.uk>
2024-11-05 20:31:35 +0000

    ameneded model for projects

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
-    thumbnail = models.URLField()  # URL to the thumbnail image
+    image = models.URLField()  # URL to the thumbnail image
+    description = models.CharField(default='No description provided.', max_length=255)
     github_url = models.URLField()  # URL to the GitHub repository
 
     def __str__(self):
--- backend/projects/serializer.py
+++ backend/projects/serializer.py
@@ -4,4 +4,4 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = '__all__'  # or specify the fields you want
+        fields = fields = ['name', 'image', 'description', 'github_url'] 
