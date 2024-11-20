7e2da8d1a6ccebcd29f4c108b463007c9f81dda7
Solmaz Purser <solmaz@hotmail.co.uk>
2024-11-20 17:07:44 +0000

    Added sanitise data for html

--- frontend/src/components/writing/postdetail.js
+++ frontend/src/components/writing/postdetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
+import DOMPurify from 'dompurify';
 
 // This is the page that contains the entire post
 const PostDetail = () => {
@@ -24,12 +25,18 @@ const PostDetail = () => {
   if (!post) return <div>Loading...</div>; // Show loading state while fetching data
 
   // Split the post body into paragraphs based on newline characters
-  const paragraphs = post.body.split('\n').filter(paragraph => paragraph.trim() !== '');
+  const SplitParagraphs= post.body.split('\n').filter(paragraph => paragraph.trim() !== '');
   // paragraph =>...: filter methods creates new array containing elements of old array passed from the earlier part of the function
   // The function paragraph => paragraph.trim() !== '' is an arrow function that takes a single parameter, paragraph.
   // trim method removes whitespace from both the beginning and the end of the string
   // !== '': This part checks if the trimmed paragraph is not equal to an empty string. If the result of trim() is not an empty string, 
   // it means that the paragraph contains some actual content (i.e., it's not just whitespace).
+    
+  // Sanitises the data passed onto it from dangerouslysetinnerhtml.Removes any potentially malicious scripts before rendering.
+  // The below is a function
+  const paragraphs = SplitParagraphs.map(paragraph =>
+    DOMPurify.sanitize(paragraph)
+  );
 
     // Handle go back to the previous page
     const handleGoBack = () => {
@@ -58,14 +65,15 @@ const PostDetail = () => {
           })}
         </p>
         {/* Render each paragraph with dangerouslySetInnerHTML */}
-        {paragraphs.map((paragraph, index) => (
+        {paragraphs.map((paragraphs, index) => (
           <p
             key={index}
-            dangerouslySetInnerHTML={{ __html: paragraph }}
+            dangerouslySetInnerHTML={{__html: paragraphs}}
             // To create the link in django admin, use this format 
             // <a href="https://www.axelos.com/resource-hub/blog/itil-4-foundation-7-tips-to-prepare" target="_blank">here</a>.
             // React dangerouslySetInnerHTML react replacement for innerHTML. It makes react aware of HTML tags in rich text editor
-            // i.e. django admin interface, and renders them properly.
+            // i.e. django admin interface, and renders them properly. Should be an object with the __html key passed the component. Only use with trusted sources as it makes the site vulnerable to XSS attacks.
+            //  
           ></p>
         ))}
       </div>
