bd0d740133f0ac8ccf862a76890317b6990d414f
Solmaz Purser <solmaz@hotmail.co.uk>
2024-11-28 20:00:46 +0000

    imported index.css from app.js rathern than index.js

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './home.css';
-
 
 function Home() {
     return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
