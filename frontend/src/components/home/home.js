836578df7ab2cd2781930d8951be99e2b5c3ef9d
Solmaz Purser <solmaz@hotmail.co.uk>
2024-11-28 22:25:58 +0000

    styling homepage

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './home.css';
 
 function Home() {
     return (
-      <div>
+      <div className="home-container">
         <h1>Solmaz Purser</h1>
         <h2>Software developer and writer</h2>
-        <Link to="/about">About</Link>
-        <p><Link to="/projects">Projects</Link></p>
-        <p><Link to="/writing">Writing</Link></p>
-        <p><Link to="/contact">Contact</Link></p>
-        <p><Link to="/todo">To do page project</Link></p>
+        <div className="links-container">
+          <Link to="/about" className="link-box link3">About</Link>
+          <Link to="/projects" className="link-box link2">Projects</Link>
+          <Link to="/writing" className="link-box link1">Writing</Link>
+          <Link to="/contact" className="link-box link4">Contact</Link>
+          <Link to="/todo" className="link-box link5">To-do Page Project</Link>
+        </div>
       </div>
-  );
-};
+    );
+}
+
 export default Home;
