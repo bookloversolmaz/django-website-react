ec47ba3b2d3ad54d747c6718e85e6a692334ade2
Solmaz Purser <solmaz@hotmail.co.uk>
2024-11-29 20:15:08 +0000

    Created colour scheme

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './home.css';
 
 function Home() {
     return (
-      <div className="home-container">
-        <h1>Solmaz Purser</h1>
-        <h2>Software developer and writer</h2>
-        <div className="links-container">
-          <Link to="/about" className="link-box link3">About</Link>
-          <Link to="/projects" className="link-box link2">Projects</Link>
-          <Link to="/writing" className="link-box link1">Writing</Link>
-          <Link to="/contact" className="link-box link4">Contact</Link>
-          <Link to="/todo" className="link-box link5">To-do Page Project</Link>
+        <div className="home-container">
+            <h1>Solmaz Purser</h1>
+            <h2>Software Developer and Writer</h2>
+            <div className="links-container">
+                <a href="/about" className="link-bubble link1">About</a>
+                <a href="/projects" className="link-bubble link2">Projects</a>
+                <a href="/writing" className="link-bubble link3">Writing</a>
+                <a href="/contact" className="link-bubble link4">Contact</a>
+                <a href="/todo" className="link-bubble link5">To do list</a>
+            </div>
         </div>
-      </div>
     );
 }
 
