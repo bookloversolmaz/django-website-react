4b488e41756dd98e6916853c511757b680d9b254
Solmaz Purser <solmaz@hotmail.co.uk>
2024-12-04 17:54:13 +0000

    Finished django build

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,13 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 application = get_asgi_application()
