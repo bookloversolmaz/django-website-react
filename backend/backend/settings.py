f8e90acfc671ed16c10cca324c0ebf890ebf5f3b
Solmaz Purser <solmaz@hotmail.co.uk>
2024-12-04 20:56:19 +0000

    issue connecting to database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,10 +36,10 @@ DATABASES = {
         'NAME': os.environ.get("DATABASE_NAME"),
         'USER': os.environ.get("DATABASE_USER"),
         'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-        # 'HOST': os.environ.get("DATABASE_HOST"),
-        # 'PORT': os.environ.get("DATABASE_PORT"),
+        'HOST': os.environ.get("DATABASE_HOST"),
+        'PORT': os.environ.get("DATABASE_PORT"),
         'TEST': {
-            # 'NAME': os.environ.get("DATABASE_TEST"),
+            'NAME': os.environ.get("DATABASE_TEST"),
         }
     } 
 }
