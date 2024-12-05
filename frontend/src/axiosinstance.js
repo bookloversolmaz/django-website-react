c730dfeb3860492749e41f40767bd2b8c794389d
Solmaz Purser <solmaz@hotmail.co.uk>
2024-12-05 23:18:01 +0000

    amended axiosinstance

--- frontend/src/axiosinstance.js
+++ frontend/src/axiosinstance.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// Set the base URL for the deployed Django backend using NODE_ENV
+// Set the base URL for the deployed Django backend using REACT_APP_URL_DEVELOPMENT and REACT_APP_URL_PRODUCTION
 let baseURL;
 
-if (process.env.REACT_APP_URL_DEVELOPMENT === 'development') {
-    baseURL = 'http://127.0.0.1:8000';  // Development URL
-} else if (process.env.REACT_APP_URL_PRODUCTION=== 'production') {
-    baseURL = 'https://solmazpurser.com';  // Production URL
+if (process.env.REACT_APP_URL_DEVELOPMENT && process.env.NODE_ENV === 'development') {
+    baseURL = process.env.REACT_APP_URL_DEVELOPMENT;  // Development URL
+} else if (process.env.REACT_APP_URL_PRODUCTION && process.env.NODE_ENV === 'production') {
+    baseURL = process.env.REACT_APP_URL_PRODUCTION;  // Production URL
 } else {
-    baseURL = 'http://127.0.0.1:8000';  // Default to development if NODE_ENV is unknown
+    baseURL = 'http://127.0.0.1:8000';  // Default to development if no specific environment variable is found
 }
 
 // Create an Axios instance with the dynamic baseURL
@@ -21,6 +21,7 @@ const AxiosInstance = axios.create({
 });
 
 export default AxiosInstance;
+
 
 
 // NODE_ENV https://create-react-app.dev/docs/adding-custom-environment-variables. 
