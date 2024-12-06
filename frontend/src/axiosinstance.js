38793e57e1e25ea05fe81887eb9c370ca71efb70
Solmaz Purser <solmaz@hotmail.co.uk>
2024-12-06 00:34:38 +0000

    amended url production in .env 4

--- frontend/src/axiosinstance.js
+++ frontend/src/axiosinstance.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-// Set the base URL for the deployed Django backend using REACT_APP_URL_DEVELOPMENT and REACT_APP_URL_PRODUCTION
-let baseURL;
+// // Set the base URL for the deployed Django backend using REACT_APP_URL_DEVELOPMENT and REACT_APP_URL_PRODUCTION
+// let baseURL;
 
-if (process.env.NODE_ENV === 'development') {
-    baseURL = process.env.REACT_APP_URL_DEVELOPMENT;  // Development URL
-} else if (process.env.NODE_ENV === 'production') {
-    baseURL = process.env.REACT_APP_URL_PRODUCTION;  // Production URL
-} else {
-    baseURL = 'http://127.0.0.1:8000';  // Default to development if no specific environment variable is found
-}
+// if (process.env.NODE_ENV === 'development') {
+//     baseURL = process.env.REACT_APP_URL_DEVELOPMENT;  // Development URL
+// } else if (process.env.NODE_ENV === 'production') {
+//     baseURL = process.env.REACT_APP_URL_PRODUCTION;  // Production URL
+// } else {
+//     baseURL = 'http://127.0.0.1:8000';  // Default to development if no specific environment variable is found
+// }
 
 // Create an Axios instance with the dynamic baseURL
 const AxiosInstance = axios.create({
