a2019702a80a8f4fb8deb0f2fa88e8d354bb4d82
Solmaz Purser <solmaz@hotmail.co.uk>
2024-12-09 15:32:27 +0000

    tidied settings

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,11 @@ environ.Env.read_env()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FRONTEND_DIR = os.path.join(BASE_DIR, '..', 'frontend', 'build')
 
+# Static files settings
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, 'build/static')]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # Set your secret key from the .env file
@@ -117,22 +122,6 @@ CORS_ALLOW_CREDENTIALS = True  # Allow credentials like cookies or authorization
 
 ROOT_URLCONF = 'backend.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [FRONTEND_DIR],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 ASGI_APPLICATION = 'backend.asgi.application'
 
 # Password validation
@@ -148,18 +137,6 @@ LANGUAGE_CODE = 'en-gb'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
-# Static files settings
-STATIC_URL = '/static/'
-
-# Static files directory to collect all static files (for production)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, 'static')]
-
-# Set up static file locations
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static'),  # React's static files
-]
 
 # Set the directory for React's index.html
 TEMPLATES = [
@@ -181,21 +158,3 @@ TEMPLATES = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
