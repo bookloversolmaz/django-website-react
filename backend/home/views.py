fd3bc25effdf64f451eb7e2045772ca99e61c6f8
Solmaz Purser <solmaz@hotmail.co.uk>
2024-12-09 15:15:16 +0000

    populated views.py in backend folder

--- backend/home/views.py
+++ backend/home/views.py
@@ -5,12 +5,12 @@ from django.urls import reverse
 class HomeView(APIView):
     def get(self, request):
         # Get the URL for the to-do list page
-        todo_page_url = reverse('todo')
+        todo_page_url = reverse('todo/index.html')
         # blog_page_url = reverse('tictactoe')
 
         # The below dictionary is the data inserted into the response
         response_data = {
-            'todo_page': todo_page_url,  # URL to the todo page
+            'todo_page/index.html': todo_page_url,  # URL to the todo page
             # 'blog_page': blog_page_url,
             # Add more links if needed
         }
--- backend/settings.py
+++ backend/settings.py
@@ -61,7 +61,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
     'backend',
     'home',
     'projects',
