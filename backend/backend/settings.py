e3c6dd9c5da0eecef6b7b21a2825c0734c328c7e
Solmaz Purser <solmaz@hotmail.co.uk>
2024-12-17 16:59:40 +0000

    created django admin superuser

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,17 +68,6 @@ DATABASES = {
     )
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DATABASE_NAME'),
-#         'USER': os.getenv('DATABASE_USER'),
-#         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-#         'HOST': os.getenv('DATABASE_HOST'),
-#         'PORT': os.getenv('DATABASE_PORT'),
-#     }
-# }
-
 STORAGE = {
     "default":{
         "BACKEND": "django.core.files.storage.FileSystemStorage",
