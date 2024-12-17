d2131e9346fe56ef6b6a6096e16eb43c116aba4b
Solmaz Purser <solmaz@hotmail.co.uk>
2024-12-17 21:00:33 +0000

    adding all files

---
+++ frontend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    transformIgnorePatterns: [
+      "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$",
+      "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$",
+      "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$"
+    ]
+  };
+  
