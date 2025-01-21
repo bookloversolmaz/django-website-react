a95f3975b23553c2b9587d3551e3df5a8ff2d239
Solmaz Purser <solmaz@hotmail.co.uk>
2025-01-21 13:11:00 +0000

    made changes to contact admin.py to relect search fileds

--- backend/contact/admin.py
+++ backend/contact/admin.py
@@ -3,6 +3,6 @@ from .models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'subject', 'created_at')  # Columns to display in the admin list view
-    search_fields = ('name', 'email', 'subject')  # Fields to include in the search bar
-    list_filter = ('created_at',)  # Filters for the sidebar
+    list_display = ('name', 'email', 'subject', 'message', 'created_at')  # Columns to display in the admin list view
+    search_fields = ('name', 'email', 'subject', 'created_at')  # Fields to include in the search bar
+    list_filter = ('name', 'email', 'subject', 'created_at')  # Filters for the sidebar
