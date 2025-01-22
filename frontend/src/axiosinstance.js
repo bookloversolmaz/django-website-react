04ec7c05934667060fa1b0df302c8e4b6f843d99
Solmaz Purser <solmaz@hotmail.co.uk>
2025-01-22 19:33:41 +0000

    defined crf token in axiosinstance

--- frontend/src/axiosinstance.js
+++ frontend/src/axiosinstance.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
-import { getCSRFToken } from './csrf';
 
-
+function getCSRFToken() {
+    // Retrieve CSRF token from cookies
+    const csrfCookie = document.cookie.match(/csrftoken=([\w-]+)/);
+    return csrfCookie ? csrfCookie[1] : '';
+}
 const baseURL = window.location.hostname === 'www.solmazpurser.com'
   ? 'https://www.solmazpurser.com'  // Production backend if the host is production, this is the baseurl
   : 'http://127.0.0.1:8000';  // Local backend for development if not, use this as the baseurl
