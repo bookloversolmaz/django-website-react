1aa50c854fef35cc91392fce4653d62ac7d2bd09
Solmaz Purser <solmaz@hotmail.co.uk>
2025-01-22 21:40:02 +0000

    added error message into axiosinstance

--- frontend/src/axiosinstance.js
+++ frontend/src/axiosinstance.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 function getCSRFToken() {
-    // Retrieve CSRF token from cookies
     const csrfCookie = document.cookie.match(/csrftoken=([\w-]+)/);
+    if (!csrfCookie) {
+        console.warn('CSRF token not found in cookies.');
+    }
     return csrfCookie ? csrfCookie[1] : '';
 }
+
 const baseURL = window.location.hostname === 'www.solmazpurser.com'
   ? 'https://www.solmazpurser.com'  // Production backend if the host is production, this is the baseurl
   : 'http://127.0.0.1:8000';  // Local backend for development if not, use this as the baseurl
