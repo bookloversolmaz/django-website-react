b54a65fc6667d78e3ba960a12527db2eee4dac8a
Solmaz Purser <solmaz@hotmail.co.uk>
2025-01-22 18:18:58 +0000

    removed getcsrftoken from axiosinstance

--- frontend/src/axiosinstance.js
+++ frontend/src/axiosinstance.js
@@ -34,14 +34,14 @@ const AxiosInstance = axios.create({
     withCredentials: true,
 });
 
-function getCSRFToken() {
-    const name = 'csrftoken';
-    const cookies = document.cookie.split(';');
-    for (let cookie of cookies) {
-        const [key, value] = cookie.trim().split('=');
-        if (key === name) return value;
-    }
-    return null;
-}
+// function getCSRFToken() {
+//     const name = 'csrftoken';
+//     const cookies = document.cookie.split(';');
+//     for (let cookie of cookies) {
+//         const [key, value] = cookie.trim().split('=');
+//         if (key === name) return value;
+//     }
+//     return null;
+// }
 
 export default AxiosInstance;
