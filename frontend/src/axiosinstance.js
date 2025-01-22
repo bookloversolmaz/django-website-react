b9addccba8c1d15b070bcda6cac8479315dbf79c
Solmaz Purser <solmaz@hotmail.co.uk>
2025-01-22 19:29:21 +0000

    imported getcsrftoken in axiosinstance

--- frontend/src/axiosinstance.js
+++ frontend/src/axiosinstance.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import { getCSRFToken } from './csrf';
+
 
 const baseURL = window.location.hostname === 'www.solmazpurser.com'
-  ? 'https://www.solmazpurser.com'  // Production backend
-  : 'http://127.0.0.1:8000';  // Local backend for development
+  ? 'https://www.solmazpurser.com'  // Production backend if the host is production, this is the baseurl
+  : 'http://127.0.0.1:8000';  // Local backend for development if not, use this as the baseurl
 
 const AxiosInstance = axios.create({
     baseURL: baseURL,
