b0a85a4473062700ea5e8234eb46d0e44b5d4768
Solmaz Purser <solmaz@hotmail.co.uk>
2025-01-26 21:37:29 +0000

    changed code to include sendgrid helpers

--- backend/contact/views.py
+++ backend/contact/views.py
@@ -1,7 +1,7 @@
 import os
 import environ
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid import SendGridAPIClient, sendgrid
+from sendgrid.helpers.mail import Mail, Email, To, Content
 from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -42,19 +42,31 @@ class ContactView(APIView):
                         status=status.HTTP_500_INTERNAL_SERVER_ERROR,
                     )
 
-                sg = SendGridAPIClient(sendgrid_api_key)
-                message = Mail(
-                    from_email=settings.DEFAULT_FROM_EMAIL,  # Use the default from email in your settings
-                    to_emails=[settings.DEFAULT_FROM_EMAIL],  # Use your personal email as recipient
-                    subject=subject,
-                    html_content=html_content,
-                )
+                # sg = SendGridAPIClient(sendgrid_api_key)
+                # message = Mail(
+                #     from_email=settings.DEFAULT_FROM_EMAIL,  # Use the default from email in your settings
+                #     to_emails=[settings.DEFAULT_FROM_EMAIL],  # Use your personal email as recipient
+                #     subject=subject,
+                #     html_content=html_content,
+                # )
+
+                sg = sendgrid.SendGridAPIClient(api_key=sendgrid_api_key)
+
+                from_email = Email("verified@example.com")
+                to_email = To("your_email@example.com")
+                subject = "SendGrid Test Email"
+                content = Content("text/plain", "This is a test email sent via SendGrid.")
+                mail = Mail(from_email, to_email, subject, content)
+
+                response = sg.client.mail.send.post(request_body=mail.get())
+                print(f"SendGrid response: {response.status_code}, {response.body.decode()}, {response.headers}")
+
 
                 # Send the email
-                response = sg.send(message)
+                # response = sg.send(message)
 
                 # Log response details for debugging
-                print(f"SendGrid response: {response.status_code} - {response.body.decode()}")
+                # print(f"SendGrid response: {response.status_code} - {response.body.decode()}")
 
                 if response.status_code == 202:
                     return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -70,6 +82,3 @@ class ContactView(APIView):
                 )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-print(f"From Email: {settings.DEFAULT_FROM_EMAIL}")
-print(f"To Email: {settings.DEFAULT_FROM_EMAIL}")
