9fb4106b648da627180185ed618ba3f9c9198153
Solmaz <solmaz@hotmail.co.uk>
2026-07-28 23:47:14 +0100

    bug hunting

--- backend/writing/admin.py
+++ backend/writing/admin.py
@@ -1,3 +1,24 @@
+# from django.contrib import admin
+# from .models import Post, ContentBlock
+
+
+# class ContentBlockInline(admin.StackedInline):
+#     model = ContentBlock
+#     extra = 1
+#     fields = (
+#         "block_type",
+#         "heading",
+#         "body",
+#         "image",
+#         "caption",
+#         "order",
+#     )
+
+
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     inlines = [ContentBlockInline]
+
 from django.contrib import admin
 from .models import Post, ContentBlock
 
@@ -5,16 +26,23 @@ from .models import Post, ContentBlock
 class ContentBlockInline(admin.StackedInline):
     model = ContentBlock
     extra = 1
-    fields = (
-        "block_type",
-        "heading",
-        "body",
-        "image",
-        "caption",
-        "order",
-    )
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     inlines = [ContentBlockInline]
+
+    def save_formset(self, request, form, formset, change):
+        print("REQUEST FILES:")
+        print(request.FILES)
+
+        instances = formset.save(commit=False)
+
+        for instance in instances:
+            print("IMAGE NAME BEFORE SAVE:")
+            print(repr(instance.image.name))
+            instance.save()
+            print("IMAGE NAME AFTER SAVE:")
+            print(repr(instance.image.name))
+
+        formset.save_m2m()
