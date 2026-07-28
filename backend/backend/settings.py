c1530b1201fd4aa9e95378597679f4629d43484a
Solmaz <solmaz@hotmail.co.uk>
2026-07-28 21:59:04 +0100

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,11 @@ TEMPLATES = [
     },
 ]
 
-CLOUDINARY_URL=env("CLOUDINARY_URL")
+cloudinary.config(
+    cloud_name=env("CLOUDINARY_CLOUD_NAME"),
+    api_key=env("CLOUDINARY_API_KEY"),
+    api_secret=env("CLOUDINARY_API_SECRET")
+)
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
