07f47d67b166b83e6f5998343e9f67ff38b35656
Solmaz <solmaz@hotmail.co.uk>
2026-07-28 20:26:35 +0100

    changed writing admin

--- backend/writing/admin.py
+++ backend/writing/admin.py
@@ -5,6 +5,14 @@ from .models import Post, ContentBlock
 class ContentBlockInline(admin.StackedInline):
     model = ContentBlock
     extra = 1
+    fields = (
+        "block_type",
+        "heading",
+        "body",
+        "image",
+        "caption",
+        "order",
+    )
 
 
 @admin.register(Post)
