2bc51bfd4763d68b4212fc3d9678c1f8f155f3a0
Solmaz <solmaz@hotmail.co.uk>
2026-07-29 00:18:32 +0100

    admin

--- backend/writing/admin.py
+++ backend/writing/admin.py
@@ -21,18 +21,25 @@
 
 from django.contrib import admin
 from .models import Post, ContentBlock
-
-
-class ContentBlockInline(admin.StackedInline):
-    model = ContentBlock
-    extra = 1
-
-
 import logging
 
 logger = logging.getLogger(__name__)
 
 
+class ContentBlockInline(admin.StackedInline):
+    model = ContentBlock
+    extra = 1
+    fields = (
+        "block_type",
+        "heading",
+        "body",
+        "image",
+        "caption",
+        "order",
+    )
+
+
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     inlines = [ContentBlockInline]
 
@@ -42,7 +49,10 @@ class PostAdmin(admin.ModelAdmin):
         instances = formset.save(commit=False)
 
         for instance in instances:
-            logger.warning("IMAGE BEFORE SAVE: %s", repr(instance.image.name))
+            logger.warning(
+                "IMAGE BEFORE SAVE: %s",
+                repr(instance.image.name)
+            )
 
             try:
                 instance.save()
@@ -57,28 +67,3 @@ class PostAdmin(admin.ModelAdmin):
                 raise
 
         formset.save_m2m()
-
-def save_formset(self, request, form, formset, change):
-    print("ENTERED SAVE_FORMSET")
-
-    try:
-        instances = formset.save(commit=False)
-        print("FORMSET SAVED")
-
-        for instance in instances:
-            print("INSTANCE:", instance)
-            print("IMAGE:", repr(instance.image.name))
-
-            instance.save()
-
-            print("SAVED OK")
-            print("IMAGE AFTER:", repr(instance.image.name))
-
-        formset.save_m2m()
-
-    except Exception:
-        import traceback
-        print("========== ERROR ==========")
-        traceback.print_exc()
-        print("============================")
-        raise
