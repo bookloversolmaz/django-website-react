482f4edb2515f9c733397f98235359fa671cc490
Solmaz <solmaz@hotmail.co.uk>
2026-07-29 13:33:25 +0100

    tesing again

--- backend/writing/admin.py
+++ backend/writing/admin.py
@@ -1,15 +1,15 @@
-from django.contrib import admin
-from .models import Post, ContentBlock
+# from django.contrib import admin
+# from .models import Post, ContentBlock
 
 
-class ContentBlockInline(admin.StackedInline):
-    model = ContentBlock
-    extra = 1
+# class ContentBlockInline(admin.StackedInline):
+#     model = ContentBlock
+#     extra = 1
 
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    inlines = [ContentBlockInline]
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     inlines = [ContentBlockInline]
 
 
 from django.contrib import admin
