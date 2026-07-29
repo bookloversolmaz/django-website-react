2ec7cee1029fcec274b9f561f59c0d59c5a5d533
Solmaz <solmaz@hotmail.co.uk>
2026-07-29 15:30:31 +0100

    Fix production Django API URL

--- frontend/src/axiosinstance.js
+++ frontend/src/axiosinstance.js
@@ -8,9 +8,9 @@ function getCSRFToken() {
     return csrfCookie ? csrfCookie[1] : '';
 }
 
-const baseURL = window.location.hostname === 'www.solmazpurser.com'
-  ? 'https://www.solmazpurser.com'  // Production backend if the host is production, this is the baseurl
-  : 'http://127.0.0.1:8000';  // Local backend for development if not, use this as the baseurl
+const baseURL = window.location.hostname === 'localhost'
+  ? 'https://django-website-react.onrender.com'
+  : 'http://127.0.0.1:8000';
 
 const AxiosInstance = axios.create({
     baseURL: baseURL,
