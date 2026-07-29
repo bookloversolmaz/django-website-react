513d1c26da87a6ad4d256482952bcc6f3aaa7910
Solmaz <solmaz@hotmail.co.uk>
2026-07-29 15:30:31 +0100

    changedchanged

--- frontend/src/axiosinstance.js
+++ frontend/src/axiosinstance.js
@@ -8,9 +8,9 @@ function getCSRFToken() {
     return csrfCookie ? csrfCookie[1] : '';
 }
 
-const baseURL = window.location.hostname === 'www.solmazpurser.com'
-  ? 'https://www.solmazpurser.com'  // Production backend if the host is production, this is the baseurl
-  : 'http://127.0.0.1:8000';  // Local backend for development if not, use this as the baseurl
+const baseURL = window.location.hostname === 'localhost'
+  ? 'http://127.0.0.1:8000'
+  : 'https://django-website-react.onrender.com';
 
 const AxiosInstance = axios.create({
     baseURL: baseURL,
